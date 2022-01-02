bc7499c81ac71269820f3100f57e9373e83b05fd
Jacky Wong <chikonwong@gmail.com>
2022-01-02 16:34:49 +0800

    Initial Commit

---
+++ Calculate Your Calorie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculate Your Calorie
+//
+//  Created by Jacky Wong on 2/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
